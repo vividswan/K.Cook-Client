0282c7a479f5a691ab3356cc2c07ed2344b1441a
kyileeyu <kyileeeyu@gmail.com>
2021-08-26 16:41:55 +0900

    Add: Footer JSX & style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,8 +10,30 @@ interface FooterProps {
 
 function Footer({ email, tel, address}: FooterProps) { 
     return (
-        <div className="Footer">
-            This is Footer
+        <div className="footer">
+            <dl>
+                <dt className="footer-title">케이쿡</dt>
+                <dd>{email}</dd>
+                <dd>{tel}</dd>
+            </dl>
+            <dl>
+                <dt className="footer-title">Services</dt>
+                <dd>Development & Strategy</dd>
+                <dd>Design & Management</dd>
+                <dd>Website Development</dd>
+                <dd>Quick Launch</dd>
+            </dl>
+            <dl>
+                <dt className="footer-title">About Us</dt>
+                <dd>Milestone</dd>
+                <dd>Check Our Pricing Plan</dd>
+                <dd>Expert Team</dd>
+                <dd>Our Exciting News</dd>
+            </dl>
+            <dl>
+                <dt className="footer-title">Address</dt>
+                <dd>{address}</dd>
+            </dl>
         </div>
     );
 }
