10ae673541ad13946eb28fd5fb67a53d125acd48
kyileeyu <kyileeeyu@gmail.com>
2021-09-08 01:34:17 +0900

    Add :  my coupon page component & styling

--- src/components/mypage/Coupons.tsx
+++ src/components/mypage/Coupons.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import '../styles/mypage/Coupon.scss';
 
 function Coupons (){
     return(
-        <div>Coupons</div>
+        <div className="mp-top coupon">
+        <div className="mypage-top coupon-top">
+            <h3>내 쿠폰 : 3장</h3>
+            <div className="coupon-code">
+                <input placeholder="할인 쿠폰 번호를 입력하세요"></input>
+                <button>등록하기</button>
+            </div>
+        </div>
+        <div className="coupon-list">
+            <div className="coupon-card">
+                <h3 className="coupon-price">30000원</h3>
+                <p className="coupon-name">오늘만 반값, 여름 특별 할인 쿠폰</p>
+                <div className="coupon-bottom">
+                    <div className="coupon-duedate">D-5</div>
+                    <button className="using-coupon">사용하러 가기 &gt;</button>
+                </div>
+            </div>
+            
+        </div>
+
+        </div>
+            
+    
     )
 }
 
--- src/components/mypage/Reserves.tsx
+++ src/components/mypage/Reserves.tsx
@@ -1,8 +1,63 @@
 import React from 'react';
+import '../styles/mypage/Membership.scss';
 
 function Reserves (){
     return(
-        <div>reserves</div>
+
+        <div className="mp-top product-review">
+            <div className="mypage-top">
+                <h3>적립금</h3>
+                <span>구매 및 후기 작성으로 현금처럼 쓸 수 있는 포인트를 받으세요</span>
+                <div className="membership">합계 : 
+                    <span className="membership-point">11,600원</span>
+                </div>
+            </div>
+            <table className="membership-table">
+                <thead className="membership-head">
+                    <tr>
+                        <th>상품명</th>
+                        <th>판매자</th>
+                        <th>날짜</th>
+                        <th>가격</th>
+                        <th>적립금</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>하트볼터치 곰돌이 케이크</td>
+                        <td>원모먼트</td>
+                        <td>2021. 03. 11</td>
+                        <td>29,000</td>
+                        <td>2,900원</td>
+                    </tr>
+                    <tr>
+                        <td>하트볼터치 곰돌이 케이크</td>
+                        <td>원모먼트</td>
+                        <td>2021. 03. 11</td>
+                        <td>29,000</td>
+                        <td>2,900원</td>
+                    </tr>
+                    <tr>
+                        <td>하트볼터치 곰돌이 케이크</td>
+                        <td>원모먼트</td>
+                        <td>2021. 03. 11</td>
+                        <td>29,000</td>
+                        <td>2,900원</td>
+                    </tr>
+                    <tr>
+                        <td>하트볼터치 곰돌이 케이크</td>
+                        <td>원모먼트</td>
+                        <td>2021. 03. 11</td>
+                        <td>29,000</td>
+                        <td>2,900원</td>
+                    </tr>
+                </tbody>
+
+            </table>
+
+            </div>
+                
+        
     )
    
 }
