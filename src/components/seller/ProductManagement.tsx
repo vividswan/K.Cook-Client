f5487f16a6b77c7901225e6636d3050289a63fb6
kyileeyu <kyileeeyu@gmail.com>
2021-09-09 18:43:40 +0900

    Modify: cake layout, seller store layout

--- src/components/seller/ProductManagement.tsx
+++ src/components/seller/ProductManagement.tsx
@@ -3,6 +3,10 @@ import '../styles/mypage/OrderHistory.scss';
 import '../styles/seller/ProductManagement.scss';
 import cake6 from   '../../assets/cake6.png';
 import { ReactComponent as AddIcon } from '../../assets/seller/add-icon.svg';
+import { ReactComponent as CloseBtn } from '../../assets/seller/closebtn.svg';
+import { ReactComponent as CopyBtn } from '../../assets/seller/copybtn.svg';
+import { ReactComponent as DragBtn } from '../../assets/seller/dragbtn.svg';
+import { ReactComponent as DragCBtn } from '../../assets/seller/drag-column-btn.svg';
 
 import { Link } from 'react-router-dom';
 
@@ -16,12 +20,11 @@ function ProductManagement (){
             <div className="management-card-list">
                 <div className="management-card">
                     <div className="management-section">
-                        <div className="move-tap"></div>
+                        <div className="move-tap"><DragBtn/></div>
                         <div className="management-content">
                             <div className="order-img">
                                 <img src={cake6}/>
                             </div>    
-                            <div className="move-column-tap"></div>
                             <div className="order-content seller-order-content">
                                 <div className="order-cake">하트볼터치 곰돌이 케이크</div>
                                 <div className="order-cake-shop">호수 : 1호, 2호, 3호, 4호</div>
@@ -31,17 +34,18 @@ function ProductManagement (){
                         </div>
                     </div>
                     <div className="management-tap">
-                        <img />
+                        <button className="delete-btn"><CloseBtn/></button>
+                        <button className="copy-btn"><CopyBtn/></button>
                     </div>
                 </div>
                 <div className="add management-card">
                     <div className="management-section">
-                        <div className="move-tap"></div>
+                        <div className="move-tap"><DragBtn/></div>
                         <div className="management-content">
                             <div className="add-img order-img">
                                 <AddIcon/>
                             </div>    
-                            <div className="move-column-tap"></div>
+                            <div className="move-column-tap"><DragCBtn/></div>
                             <div className="order-content seller-order-content">
                                 <input className="add-cake-name" placeholder="옵션 이름"></input>
                                 <div className="add-option">
@@ -51,10 +55,13 @@ function ProductManagement (){
                         </div>
                     </div>
                     <div className="management-tap">
-                        <button className="delete-btn"></button>
-                        <button className="copy-btn"></button>
+                        <button className="delete-btn"><CloseBtn/></button>
+                        <button className="copy-btn"><CopyBtn/></button>
                     </div>
                 </div>
+            </div>
+            <div className="option-add-btn">
+                <AddIcon/>
             </div>
             
         </div>
